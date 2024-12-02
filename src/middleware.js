97ea89f935e777e66d2dec1fa874a4558c1e9bb1
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2024-12-02 15:18:46 -0800

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,4 @@
 export async function onRequest (context, next) {
-    // intercept data from a request
-    // optionally, modify the properties in `locals`
     context.locals.title = "New title";
-
-    // return a Response or the result of calling `next()`
     return next();
 };
